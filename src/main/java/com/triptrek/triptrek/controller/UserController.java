22861fa4eb54abb0fda5ab8326bb8c07bfde8028
joostwijg <joost@webwij.nl>
2025-01-16 08:32:07 +0100

    RegisterRequest made

--- src/main/java/com/triptrek/triptrek/controller/UserController.java
+++ src/main/java/com/triptrek/triptrek/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.triptrek.triptrek.controller;
 
 
+import com.triptrek.triptrek.dto.RegisterRequest;
 import com.triptrek.triptrek.model.User;
 import com.triptrek.triptrek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +10,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin (origins = "http://localhost:3000")
+@CrossOrigin (origins = "http://localhost:5177")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request) {
         try {
+            // Controleer of het wachtwoord en confirmPassword overeenkomen
+            if (!request.getPassword().equals(request.getConfirmPassword())) {
+                return ResponseEntity.badRequest().body("Passwords do not match!");
+            }
+
+            // Maak een nieuwe gebruiker op basis van de aanvraag
+            User user = new User();
+            user.setEmail(request.getEmail());
+            user.setPassword(request.getPassword());
+
+            // Registreer de gebruiker
             User registeredUser = userService.registerUser(user);
             return ResponseEntity.ok(registeredUser);
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
 
     @PostMapping("/login")
     public User loginUser(@RequestBody User loginDetails) {
